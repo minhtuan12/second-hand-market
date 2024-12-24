a0154ce91bbf4b9213ff82d8339374f5a7120d79
Nguyễn Minh Tuân <minhtuanng12@gmail.com>
2024-12-24 14:59:59 +0700

    fix sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,23 @@
 /** @[action] {import('next').NextConfig} */
 
-const nextConfig = {
+import sitemap from "./next-sitemap.config.js";
+
+const nextConfig = sitemap({
     reactStrictMode: false,
     env: {
         API_URL: process.env.API_URL,
-        APP_ENV: process.env.APP_ENV
+        APP_ENV: process.env.APP_ENV,
     },
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com'
-            }
-        ]
-    }
-};
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+            },
+        ],
+    },
+    siteUrl: "https://chodocu.vercel.app",
+    generateRobotsTxt: true,
+});
 
 export default nextConfig;
